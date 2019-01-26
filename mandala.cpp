ed6257dea825cb5ed19502eb5e1103429b937753
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2019-01-26 23:40:13 +0530

    Board Design complete

--- mandala.cpp
+++ mandala.cpp
@@ -8,54 +8,14 @@ using namespace cv;
 using namespace std;
 Mat original(ROW, COLUMN, CV_8UC3);
 VideoCapture cap("v4l2:///dev/video0");
-// void drawReflection(vector < pair < int, int > > points, lineEquation le,
-// 	Mat obj, Vec3b color)
-// {
-	
-// }
-void drawBoard(Mat obj, int depth, Vec3b color)
-{
-	double stepSize = 45.0/pow(2,depth);
-	double mDeg =0.0;
-	vector <lineEquation> le;
-	le.push_back(lineEquation(1, 1, 0));
-	le.push_back(lineEquation(0, 1, 0));
-	le.push_back(lineEquation(1, 0, 0));
-	for(int i =0; i!=le.size(); i++)
-	{
-		le[i].displayVal();
-	}
-	while(mDeg <= 45)
-	{
-		cout<<mDeg<<" ";
-		double mRad = degreeToRadian(mDeg);
-		cout<<mRad<<" "<<tan(mRad)<<endl;
-		int inp;
-		pair <int, int> a = translate(make_pair(
-			double(tan(mRad) * int(BOARD_WIDTH/2)), BOARD_WIDTH/2));
-		pair <int, int> b = translate(make_pair(0,0));
-		vector <pair <int, int>> linePoints = getLine(a,b);	
-		// for(int i=0; i<line.size(); i++)
-		// {
-		// 	cout<<line[i].first<<" "<<line[i].second<<endl;
-		// }
-		drawPoints(linePoints, obj, COLOR_BOARD);
-		drawReflection(linePoints, le, obj, COLOR_BOARD);
-		mDeg+=stepSize;
-		// 	cin>>inp;
-	}
-	for(int i=11; i<40; i++)
-	{
-		obj.at<Vec3b>(i, i+5)=COLOR_BACKGROUND;
-	}
 
-}
 int main()
 {
 	namedWindow("canavas", WINDOW_NORMAL);
 	namedWindow("original", WINDOW_NORMAL);
 	Mat canavas(ROW, COLUMN, CV_8UC3);
-	int depth = 2;
+	int depth = 5;
+	depth-=2;
 	if(cap.isOpened()==false)
 	{
 		cout<<"Cannot open the video file"<<endl;
@@ -67,13 +27,13 @@ int main()
 		0,
 		COLUMN, 
 		canavas, 
+		COLOR_BLACK);
+	initializeMatObject(BOARD_ROW, 
+		BOARD_ROW + BOARD_HEIGHT,
+		BOARD_COLUMN,
+		BOARD_COLUMN + BOARD_WIDTH, 
+		canavas, 
 		COLOR_BACKGROUND);
-	// initializeMatObject(BOARD_ROW, 
-	// 	BOARD_ROW + BOARD_HEIGHT,
-	// 	BOARD_COLUMN,
-	// 	BOARD_COLUMN + BOARD_WIDTH, 
-	// 	canavas, 
-	// 	COLOR_BACKGROUND);
 	imshow("canavas", canavas);
 	drawBoard(canavas, 
 			depth, 
--- mandalalib.h
+++ mandalalib.h
@@ -21,7 +21,7 @@ typedef struct lineEquation
 		cout<<a<<" "<<b<<" "<<c<<endl;
 	}
 }lineEquation;
-pair <int, int> translate(pair <int, int> p)
+pair <int, int> translateMandalaToOpenCV(pair <int, int> p)
 {
 	/*
 		convert a coordinate from mandala coordinate syatem to opencv 
@@ -31,8 +31,83 @@ pair <int, int> translate(pair <int, int> p)
 	return make_pair(BOARD_ROW + (p.first -BOARD_HEIGHT/2)*-1,
 		p.second + BOARD_COLUMN + BOARD_WIDTH/2);
 }
+pair <int, int> translateOpenCVToMandala(pair <int, int> p)
+{
+	/*
+		convert a coordinate from opencv to mandala coordinate syatem
+		coordinate system
+		param1: .first = row, .seoond = column
+	*/
+	return make_pair((p.first - BOARD_ROW - BOARD_HEIGHT/2)*-1,
+		p.second - BOARD_COLUMN - BOARD_WIDTH/2);
+}
 double degreeToRadian(double degree)
 {
 	return (degree/180)*PI;
 }
-
+vector < pair <int,int> > getReflectedLine(vector < pair< int, int> > points, lineEquation le)
+{
+	vector < pair <int,int> > reflectedPoints;
+	int a = le.a, b=le.b, c=le.c;
+	cout<<"a="<<a<<" "<<b<<" "<<c<<endl;
+	
+	for(int i=0; i!=points.size(); i++)
+	{
+		pair < int, int> mandalaPoint = translateOpenCVToMandala(points[i]);
+		int q=mandalaPoint.first;
+		int p=mandalaPoint.second;
+		cout<<p<<" "<<q<<endl;
+		int x=(((p * ((a*a) - (b*b))) - (2*b *((a*q) + c)))) / ((a*a) + (b*b));
+		cout<<x<<endl;
+		int y=(((q * ((b*b) - (a*a))) - (2*a *((b*p) + c)))) / ((a*a) + (b*b));
+		reflectedPoints.push_back(translateMandalaToOpenCV(make_pair(y, x)));
+	}
+	return reflectedPoints;
+}
+void drawBoard(Mat obj, int depth, Vec3b color)
+{
+	vector <pair <int, int>> allBoardPoints;
+	double stepSize = 45.0/pow(2,depth);
+	double mDeg =0.0;
+	vector <lineEquation> le;
+	le.push_back(lineEquation(1, -1, 0));
+	le.push_back(lineEquation(0, 1, 0));
+	le.push_back(lineEquation(1, 0, 0));
+	for(int i =0; i!=le.size(); i++)
+	{
+		le[i].displayVal();
+	}
+	while(mDeg <= 45)
+	{
+		cout<<mDeg<<" ";
+		double mRad = degreeToRadian(mDeg);
+		cout<<mRad<<" "<<tan(mRad)<<endl;
+		int inp;
+		pair <int, int> a = translateMandalaToOpenCV(make_pair(
+			double(tan(mRad) * int(BOARD_WIDTH/2)), BOARD_WIDTH/2));
+		pair <int, int> b = translateMandalaToOpenCV(make_pair(0,0));
+		vector <pair <int, int>> linePoints = getLine(a,b);	
+		// for(int i=0; i<line.size(); i++)
+		// {
+		// 	cout<<line[i].first<<" "<<line[i].second<<endl;
+		// }
+		drawPoints(linePoints, obj, COLOR_BOARD);
+		allBoardPoints.insert(allBoardPoints.end(),
+			linePoints.begin(), linePoints.end());
+		cout<<allBoardPoints.size()<<endl;
+		mDeg+=stepSize;
+	}
+	for(int i=0; i!=le.size();i++)
+	{
+		vector < pair < int, int> > linePoints = 
+			getReflectedLine(allBoardPoints, le[i]);
+		// for(int i=0; i<linePoints.size(); i++)
+		// {
+		// 	cout<<linePoints[i].first<<" "<<linePoints[i].second<<endl;
+		// }
+		drawPoints(linePoints, obj, color);
+		allBoardPoints.insert(allBoardPoints.end(),
+			linePoints.begin(), linePoints.end());
+		cout<<allBoardPoints.size()<<endl;
+	}
+}
